9f2c26f01e897a63a01ba1e497a548bed8bf21c0
cullyfung <fcx.cli@qq.com>
2022-10-06 23:14:12 +0800

    chore: init eslint+husky+prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const { resolve } = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': resolve(__dirname)
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
